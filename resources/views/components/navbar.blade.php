ec49ddd4849f7e47c2d8ab3408c9668eadf65d2d
andirchmd <andirchmd0@gmail.com>
2023-10-19 16:05:46 +0800

    updates?

---
+++ resources/views/components/navbar.blade.php
@@ -0,0 +1,23 @@
+<!-- Navbar -->
+<nav class="bg-[#B27423] border-gray-400">
+    <div class="mx-auto flex max-w-screen-xl flex-wrap items-center justify-between px-4 py-2">
+        <a href="#" class="flex items-center">
+            <img src="https://placehold.co/300x300" class="mr-3 h-7" alt="Logo Timeless Brace" />
+            <span class="self-center whitespace-nowrap text-xl font-semibold text-white">Timeless Brace</span>
+        </a>
+        <ul class="text-white flex flex-row gap-12 p-2">
+            <li>
+                <a href="#" class="underline-offset-4 transition-all hover:underline">Home</a>
+            </li>
+            <li>
+                <a href="#products" class="underline-offset-4 transition-all hover:underline">Products</a>
+            </li>
+            <li>
+                <a href="#" class="underline-offset-4 transition-all hover:underline">About</a>
+            </li>
+            <li>
+                <a href="#" class="underline-offset-4 transition-all hover:underline">Contact</a>
+            </li>
+        </ul>
+    </div>
+</nav>
---
+++ resources/views/components/products.blade.php
@@ -0,0 +1,38 @@
+<!-- Products -->
+<section id="products" class="text-[#B27423] flex h-[500px] flex-col items-center bg-white">
+    <h4 class="pb-5 text-center text-2xl font-bold">FEATURED PRODUCTS</h4>
+    <div class="container grid grid-cols-4 gap-8 px-8">
+        <a href="#" class="h-auto w-[250px] max-w-xs rounded-xl shadow-lg">
+            <img class="w-full rounded-xl transition-all hover:scale-125"
+                src="{{ 'assets/images/prw-35-1a.png' }}" />
+            <div class="py-2">
+                <div class="mb-2 text-center text-xl font-semibold">PRW-35-1A</div>
+                <p class="text-center text-lg text-gray-700">Rp 1.289.000</p>
+            </div>
+        </a> 
+        <div class="h-auto w-[250px] max-w-xs rounded-xl shadow-lg">
+            <img class="w-full rounded-xl"
+                src="{{ 'assets/images/prw-35y-1b.png' }}"/>
+            <div class="py-2">
+                <div class="mb-2 text-center text-xl font-semibold">PRW-35Y-1B</div>
+                <p class="text-center text-lg text-gray-700">Rp 1.399.000</p>
+            </div>
+        </div>
+        <div class="h-auto w-[250px] max-w-xs rounded-xl shadow-lg">
+            <img class="w-full rounded-xl"
+                src="{{ 'assets/images/romans-queen.png' }}"/>
+            <div class="py-2">
+                <div class="mb-2 text-center text-xl font-semibold">Romans Queen</div>
+                <p class="text-center text-lg text-gray-700">Rp 749.000</p>
+            </div>
+        </div>
+        <div class="h-auto w-[250px] max-w-xs rounded-xl shadow-lg">
+            <img class="w-full rounded-xl"
+                src="{{ 'assets/images/king-romans.png' }}"/>
+            <div class="py-2">
+                <div class="mb-2 text-center text-xl font-semibold">King Romans</div>
+                <p class="text-center text-lg text-gray-700">Rp 889.000</p>
+            </div>
+        </div>
+    </div>
+</section>
